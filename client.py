d42df89d42af663c62c8cfbaec3ee4ed5a1448e8
Steven Shen <stevenshen1999@hotmail.com>
2020-08-06 20:19:18 +0930

    Finished writing the assignment, started work on writing the report

--- client.py
+++ client.py
@@ -15,6 +15,9 @@ if len(sys.argv) != 4:
     print("Usage: python3 client.py server_ip server_port client_udp_port")
     exit(0)
 
+# For when the automatic scrubber removes outdated beacons
+file_lock = Lock()
+
 #Server would be running on the same host as Client
 serverName = sys.argv[1]
 serverPort = int(sys.argv[2])
@@ -24,42 +27,69 @@ clientUDPport = int(sys.argv[3])
 clientSocket_p2p = socket(AF_INET, SOCK_DGRAM)
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-serverSocket.bind(('localhost', clientUDPport)) 
+
+# Assuming that the server is running on the server IP as the client
+serverSocket.bind((serverName, clientUDPport)) 
 
 # TCP Connection
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 
-''' Don't need to touch this again, this is the initial login logic '''
+# No matter what, we always ask for the login details when spinning up a client instance
 username = input("Username: ")
 password = input("Password: ")
 
+# Login this current client (or at least attempt to)
 initial_payload = {"operation": "login", "username": username, "password": password}
 clientSocket.send(dumps(initial_payload).encode('utf-8'))
-valid_commands = {"Download_tempID", "logout", "Upload_contact_log"}
-logged_out = False
-tempID = {'id': '0000000', 'start': '000000', 'end': '000000'}
 
-# TODO: With time, refactor these error message codes and statuses into integers
+# Some global variables to help with the TCP logic later
+valid_commands = {"Download_tempID", "logout", "Upload_contact_log"}
+
+# To exit the client
+logged_out = False
+
+# To store the tempID downloaded from the server, otherwise, a dict of 0s
+tempID = {'id': '0000000', 'start': '000000', 'end': '000000', 'protocol': '1'}
+statuses = {
+    "blocked_0": "Invalid Password. Your account has been blocked. Please try again later",
+    "blocked_1": "Your account is blocked due to multiple login failures. Please try again later",
+    "Not a valid user": "Account does not exist",
+    "logged_out": None
+}
+
+# Three functions in this client.py logic
+
+# This function handles the TCP connections
 def tcp_handler():
     global clientSocket
     global valid_commands
     global logged_out
     global tempID
+
+    # A special case, since for p2p connections, no responses will be observed
     previous_command_p2p = False
 
     while(1):
         # TCP Server/client traffic
+        # If the previous command was a p2p command (i.e. Beacon), we skip over the TCP logic
         if not previous_command_p2p:
+            # Receive the server responses
             message = clientSocket.recv(2048)
-            print(message)
+
+            # Serialise response into python object
             message = loads(message)
             local_payload = {}
 
-            if (message['status'] == ""):
-                print("Something else went wrong")
+            # Handle the various messages
+            if (message['status'] in statuses):
+                if (statuses[message['status']]):
+                    print(statuses[message['status']])
                 logged_out = True
                 break
+
+            # Incorrect password logic, note that if this user is blocked, it will enter the condition above
+            # and exit this client
             elif (message['status'] == "incorrect"):
                 print("Invalid Password. Please try again")
                 password = input("Password: ")
@@ -67,57 +97,67 @@ def tcp_handler():
                 local_payload = {"operation": "login", "username": username, "password": password}
                 clientSocket.send(dumps(local_payload).encode('utf-8'))
                 continue
-            elif (message['status'] == "blocked_0"):
-                print("Invalid Password. Your account has been blocked. Please try again later")
-                break
-            elif (message['status'] == "blocked_1"):
-                print("Your account is blocked due to multiple login failures. Please try again later")
-                break
+
+            # We are logged in, can start listening to commands from the client
             elif (message['status'] == "logged_in"):
                 print("Welcome to the BlueTrace Simulator!")
-            elif (message['status'] == "logged_out"):
-                logged_out = True
-                break
-            elif (message['status'] == "success"):
-                if ('id' in message):
-                    print(f"TempID: {message['id']}")
-                    # Storing this for the beacon
-                    curr_time = dt.datetime.now()
-                    tempID['id'] = message['id']
-                    tempID['start'] = curr_time.strftime("%d/%m/%Y %H:%M:%S")
-                    tempID['end'] = (curr_time + dt.timedelta(minutes=3)).strftime("%d/%m/%Y %H:%M:%S")
 
+            # THis is the case when the tempID is successfully generated and returned to the client
+            elif (message['status'] == "success"):
+                print(f"TempID: {message['id']}")
+
+                # Storing this tempID for the p2p beacon information
+                curr_time = dt.datetime.now()
+                tempID['id'] = message['id']
+                tempID['start'] = curr_time.strftime("%d/%m/%Y %H:%M:%S")
+                tempID['end'] = (curr_time + dt.timedelta(minutes=3)).strftime("%d/%m/%Y %H:%M:%S")
+
+            # This is the case when the upload contact log was successful
+            elif (message['status'] == "success_1"):
+                # No printout required
+                pass
+
+        # Reset this variable
         previous_command_p2p = False
 
-        command = input("")
-        while (command not in valid_commands and command.find("Beacon") == -1):
-            print("Error. Invalid command")
-            command = input("")
+        command = input("> ")
+        # We keep on repeating this input() until the user inputs a valid command
+        while (command not in valid_commands):
+            if (command.find("Beacon") != -1):
+                command = command.split(" ")
+                if (len(command) != 3):
+                    print("Error, usage: Beacon targetIP targetPort")
+                else:
+                    break
+            else:
+                print("Error. Invalid command")
+            command = input("> ")
 
+        # We can most likely improve the performance using say integer flags, but I ran out of time :(
         local_payload = {"operation": command}
 
         if (command == "Upload_contact_log"):
-            # TODO: Change this to the actual contact log (depending on how p2p is implemented)
+            # Need to extract the information from the contactlog file
             with open("./z5161616_contactlog.txt") as file:
                 contacts = file.readlines()
-                # Read in all the contacts
+
+                # Read in all the contacts, and do some preprocessing, i.e. remove trailing \n 
                 contacts = [i.strip().split(" ") for i in contacts]
+
                 for i in contacts:
                     print(f"{i[0]}, {i[1]} {i[2]}, {i[3]} {i[4]};")
 
+                # Attach the list to the payload
                 local_payload["payload"] = contacts
-        elif (command.find("Beacon") != -1):
-            # First parse the information
-            command = command.split(" ")
-            if (len(command) != 3):
-                # TODO: Handle the situation where this send beacon command doesn't have the right syntax
-                pass
 
-            p2p_sender(command[1], int(command[2]), tempID)
+        # This means we are using a p2p command
+        elif (command[0] == "Beacon"):
             previous_command_p2p = True
+
+            # Send the specified address + port combo this particular beacon information
+            clientSocket_p2p.sendto(dumps(tempID).encode(), (command[1], int(command[2]),))
             continue
 
-        print(local_payload)
         clientSocket.send(dumps(local_payload).encode('utf-8'))
 
     clientSocket.close()
@@ -134,15 +174,19 @@ def p2p_receiver():
         invalid = True
         current_time = dt.datetime.now()
 
+        # Attempt to decode the message sent 
         try:
             start = dt.datetime.strptime(p2p_message['start'], "%d/%m/%Y %H:%M:%S") 
-            end =  dt.datetime.strptime(p2p_message['end'], "%d/%m/%Y %H:%M:%S")
+            start -= dt.timedelta(microseconds=start.microsecond)
+            end = dt.datetime.strptime(p2p_message['end'], "%d/%m/%Y %H:%M:%S")
+            end -= dt.timedelta(microseconds=end.microsecond)
 
             if (start <= current_time <= end):
                 # valid time stamp, write to the contact log
                 invalid = False
         except ValueError as e:
-            continue
+            start = p2p_message['start']
+            end = p2p_message['end']
 
 
         print(f"received beacon: {p2p_message['id']}, {p2p_message['start']}, {p2p_message['end']}.")
@@ -153,28 +197,31 @@ def p2p_receiver():
             with open("./z5161616_contactlog.txt", "a") as file:
                 file.writelines(f"{p2p_message['id']} {p2p_message['start']} {p2p_message['end']}\n")
 
-# This gets initialised in the tcP_handler function, we only initialise this after the client log in
-# and wants to send a beacon to someone
-def p2p_sender(address, port, payload):
-    global serverSocket
-    global clientSocket_p2p
 
-    # Send the tempID, the two timestamps to the target address/port
-    clientSocket_p2p.sendto(dumps(payload).encode(), (address, port,))
+# This function wakes up every 30 seconds to remove contact log beacons older than 3 minutes
+def contact_log_scrubber():
+    while 1:
+        file_lock.acquire()
+        with open("./z5161616_contactlog.txt", "r") as file:
+            contacts = file.readlines()
+            contacts = [i.strip().split(" ") for i in contacts]
+            contacts = [i for i in contacts if (dt.datetime.strptime(i[1] + " " + i[2], "%d/%m/%Y %H:%M:%S") + dt.timedelta(minutes=3) > dt.datetime.now())]
 
-    # TODO: Handle the case when the sending was not successful
+        with open("./z5161616_contactlog.txt", "w") as file:
+            for i in contacts:
+                file.writelines(' '.join(i) + "\n")
 
+        file_lock.release()
 
-# TODO: Add a function here that reads the contactlog every 1 min and remove beacons older than 3 mins
-# use the threading module to achieve this
+        sleep(30)
 
 def main():
     start_new_thread(tcp_handler, ())
     start_new_thread(p2p_receiver, ())
+    start_new_thread(contact_log_scrubber, ())
 
     while 1:
         if (logged_out):
-            print("Entered here")
             break
         sleep(0.1)
 
--- server.py
+++ server.py
@@ -19,31 +19,34 @@ import sys
 if len(sys.argv) != 3:
     print("Usage: python3 server.py server_port block_duration")
 
+# Initialising some global variables
 unsuccessful_logins = {}
 logins = {}
 file_lock = threading.Lock()
-block_duration = sys.argv[2]
+block_duration = int(sys.argv[2])
 
 # Load in the credentials.txt file
+# Since we don't have to create new accounts, just need to read in credentials once
 with open("./credentials.txt", "r") as file:
     for i in file.readlines():
         i = i.strip().split(" ")
         logins[i[0]] = i[1]
 
 def threaded_client(connectionSocket):
+    # The client that this particular thread is servicing
     local_user = None
+
     global unsuccessful_logins
     global logins
+
     while True:
         sentence = connectionSocket.recv(1024)
 
         sentence = loads(sentence)
-        print(sentence)
 
         message = ""
         payload = None
         if (sentence['operation'] == 'login'):
-            print("Entered this bit")
             # First check if the account is currently locked out
             if (sentence['username'] in unsuccessful_logins):
                 if (not isinstance(unsuccessful_logins[sentence['username']], int)):
@@ -56,16 +59,18 @@ def threaded_client(connectionSocket):
                         unsuccessful_logins.pop(sentence['username'])
 
             if (sentence['username'] not in logins):
-                message = ""
+                message = "Not a valid user"
             else:
+                # Check the passwords against the stored login information
                 if (sentence['password'] == logins[sentence['username']]):
                     message = "logged_in"
                     local_user = sentence['username']
                 else:
                     if (sentence['username'] in unsuccessful_logins):
                         unsuccessful_logins[sentence['username']] += 1
+                        # Lock the account if this is the third time the user has inputted a wrong password
                         if (unsuccessful_logins[sentence['username']] == 3):
-                            # TODO: Change this to block_time once we integrate the commandline
+                            # Set a locked until time
                             unsuccessful_logins[sentence['username']] = dt.datetime.now() + dt.timedelta(seconds=block_duration)
                             message = "blocked_0"
                             break
@@ -74,77 +79,84 @@ def threaded_client(connectionSocket):
                     message = "incorrect"
         elif (sentence['operation'] == 'logout'):
             message = "logged_out"
+            print(f"> {local_user} logout")
             break
         elif (sentence['operation'] == 'Download_tempID'):
+            print(f"> user: {local_user}")
             file_lock.acquire()
+
+            # Generate a random 20 bytes ascii string
             temp_id = ''.join(choices(ascii_lowercase+ascii_uppercase+digits, k=20))
+
+            # Write to the temPID logfile the tempID generated time and also expiry time
             current_time = dt.datetime.now()
             current_time = current_time.strftime("%d/%m/%Y %H:%M:%S")
             expiry_time = dt.datetime.now() + dt.timedelta(minutes=15)
             expiry_time = expiry_time.strftime("%d/%m/%Y %H:%M:%S")
 
+            # Write to the file
             with open("./tempIDs.txt", "a") as file:
                 file.writelines(f"{local_user} {temp_id} {current_time} {expiry_time}\n")
-            file_lock.release()
-            message = "success"
-            payload = {"id": temp_id}
-        elif (sentence['operation'] == 'Upload_contact_log'):
-            # FIXME: Have I got the contacts_to_check and tempIDs the other way around?
 
+            file_lock.release()
+
+            message = "success"
+            print(f"> TempID: {temp_id}")
+            payload = {"id": temp_id}
+
+        elif (sentence['operation'] == 'Upload_contact_log'):
             # Retrieve the contact log
             contacts_to_check = {}
-            print(f"received contact log from {local_user}")
+            print(f"> received contact log from {local_user}")
+
             for i in sentence['payload']:
+                # Go through the payload and convert the uploaded contacg log to native python datetime objects
                 print(f"{i[0]}, {i[1]} {i[2]}, {i[3]} {i[4]};")
                 contacts_to_check[i[0]] = {}
-                print(i[1] + " " + i[2])
                 contacts_to_check[i[0]]['start'] = dt.datetime.strptime(i[1] + " " + i[2], "%d/%m/%Y %H:%M:%S")
                 contacts_to_check[i[0]]['end'] = dt.datetime.strptime(i[3] + " " + i[4], "%d/%m/%Y %H:%M:%S")
 
             # Trace
-            print("Contact log checking")
+            print("> Contact log checking")
 
-            # First get a list of all the tempIDs
             file_lock.acquire()
+            # First get a list of all the tempIDs
             with open("./tempIDs.txt", "r") as file:
                 tempIDs = file.readlines()
+
+                # Some preprocessing to remove the trailing \n and also split the string
                 tempIDs = [i.strip().split(" ") for i in tempIDs]
                 for i in tempIDs:
                     temp_id = i[1]
+
+                    # Convert the stored timestamps in the txt file into native python datetime objects
                     time = dt.datetime.strptime(i[2] + " " + i[3], "%d/%m/%Y %H:%M:%S")
 
-                    # I don't know if we need to check the alternative (i.e. the temp_id in the contactlog is fake)
+                    # Check if this particular tempID needs to be checked
                     if (temp_id in contacts_to_check):
+
+                        # Verify the timestamp validity
                         if (contacts_to_check[temp_id]['start'] <= time <= contacts_to_check[temp_id]['end']):
                             print(f"{i[0]}, {contacts_to_check[temp_id]['start']};")
 
             file_lock.release()
 
             numbers = {}
-            for i in sentence['payload']:
-                print(i)
-            message = "success"
+            message = "success_1"
 
+        # Serve server responses back to the client
         if (not payload):
-            connectionSocket.send(dumps({"status": message}).encode('utf-8'))
+            connectionSocket.send(dumps({"status": message}).encode())
         else:
             payload["status"] = message
-            connectionSocket.send(dumps(payload).encode('utf-8'))
+            connectionSocket.send(dumps(payload).encode())
         
 
     # This gets reached either by error states (i.e. locked out by three incomplete passwords, lock out time not reached)
-    connectionSocket.send(dumps({"status": message}).encode('utf-8'))
+    connectionSocket.send(dumps({"status": message}).encode())
     connectionSocket.close()
 
-        
-
-#Define connection (socket) parameters
-#Address + Port no
-#Server would be running on the same host as Client
-# change this port number if required
-
 def main():
-    # TODO: Change this to dynamic
     serverPort = int(sys.argv[1])
 
     serverSocket = socket(AF_INET, SOCK_STREAM)
@@ -153,8 +165,8 @@ def main():
 
     serverSocket.listen(1)
 
-    print("The server is ready to receive")
-
+    # The server will spin up a new thread for each incoming connection, allowing for multiple clients
+    # to access the server concurrently
     while 1:
         connectionSocket, addr = serverSocket.accept()
         start_new_thread(threaded_client, (connectionSocket,))
